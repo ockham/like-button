cdf1b59a96e507f368637029a048c1a61bf91524
Bernie Reiter <ockham@raz.or.at>
2023-10-02 14:21:49 +0200

    Make work without Interactivity API

--- src/render.php
+++ src/render.php
@@ -1,58 +1,16 @@
 <?php
-/**
- * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
- */
-$heart_icon         = file_get_contents( plugin_dir_path( __DIR__ ) . 'public/images/empty-heart.svg' );
-$wrapper_attributes = get_block_wrapper_attributes();
-
-$likedComments = array();
-$likedPosts    = array();
-
-wp_store(
-	array(
-		'state'     => array(
-			'likes' => array(
-				'likedComments' => $likedComments,
-				'likedPosts'    => $likedPosts,
-			),
-		),
-		'selectors' => array(
-			'likes' => array(
-				'isLiked' => false,
-			),
-		),
-	),
-);
-
-if ( ! empty( $block->context['commentId'] ) ) {
-	$commentId = $block->context['commentId'];
-	$context = '{ "comment": { "id": ' . $commentId . ' } }';
-} else if ( ! empty( $block->context['postId'] ) ) {
-	$postId = $block->context['postId'];
-	$context = '{ "post": { "id": ' . $postId . ' } }';
-} else {
-	$context = '';
-}
-
+$heart_icon = file_get_contents( plugin_dir_path( __DIR__ ) . 'public/images/empty-heart.svg' );
 ?>
 
 <div
-	<?php echo $wrapper_attributes; ?>
-	data-wp-interactive
-	data-wp-context='<?php echo $context; ?>'
+	<?php echo get_block_wrapper_attributes(); ?>
 >
-	<div
-			class="like-button-parent"
-			data-wp-on--click="actions.likes.toggle"
-		>
-			<div
-				class="like-button-child"
-				data-wp-class--liked="selectors.likes.isLiked"
-			>
-				<?php echo $heart_icon; ?>
-				<span>
-					<?php _e( 'Like', 'like-button' ); ?>
-				</span>
-			</div>
+	<div class="like-button-parent" >
+		<div class="like-button-child">
+			<?php echo $heart_icon; ?>
+			<span>
+				<?php _e( 'Like', 'like-button' ); ?>
+			</span>
+		</div>
 	</div>
 </div>
--- src/view.js
+++ src/view.js
@@ -1,39 +1,10 @@
 /**
  * WordPress dependencies
  */
-import { store } from '@wordpress/interactivity';
+const likeButtons = document.querySelectorAll('.like-button-parent');
 
-store({
-	selectors: {
-		likes: {
-			isLiked: ({ state, context }) => {
-				if ( context?.comment?.id ) {
-					return state.likes.likedComments.includes(context.comment.id)
-				} else if ( context?.post?.id ) {
-					return state.likes.likedPosts.includes(context.post.id)
-				}
-			}
-		},
-	},
-	actions: {
-		likes: {
-			toggle: ({ state, context }) => {
-				if ( context?.comment?.id ) {
-					const index = state.likes.likedComments.findIndex(
-						(post) => post === context.comment.id
-					);
-					if (index === -1)
-						state.likes.likedComments.push(context.comment.id);
-					else state.likes.likedComments.splice(index, 1);
-				} else if ( context?.post?.id ) {
-					const index = state.likes.likedPosts.findIndex(
-						(post) => post === context.post.id
-					);
-					if (index === -1)
-						state.likes.likedPosts.push(context.post.id);
-					else state.likes.likedPosts.splice(index, 1);
-				}
-			},
-		},
-	},
-});
+likeButtons.forEach( likeButton => {
+	likeButton.onclick = function() {
+		likeButton.classList.toggle( 'liked' );
+	}
+} );
