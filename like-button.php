6bba24d795b3456aa1875845a5a334edcb9127bb
Bernie Reiter <ockham@raz.or.at>
2024-01-10 14:23:59 +0100

    Try hooked_block filter with Group block wrapper

--- like-button.php
+++ like-button.php
@@ -24,3 +24,41 @@ function create_block_like_button_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
 add_action( 'init', 'create_block_like_button_block_init' );
+
+function insert_like_button_after_post_content( $hooked_block, $position, $anchor_block ) {
+	if ( 'before' !== $position && 'after' !== $position ) {
+		return $hooked_block;
+	}
+
+	if ( 'core/post-content' !== $anchor_block['blockName'] ) {
+		return $hooked_block;
+	}
+
+	$attrs = isset( $anchor_block['attrs']['layout']['type'] )
+		? array(
+			'layout' => array(
+				'type' => $anchor_block['attrs']['layout']['type']
+			)
+		)
+		: array();
+
+	// Wrap the Like Button block in a Group block.
+	return array(
+		'blockName' => 'core/group',
+		'attrs'     => $attrs,
+		'innerBlocks' => array(
+			array(
+				'blockName'    => 'ockham/like-button',
+				'attrs'        => array(),
+				'innerBlocks'  => array(),
+				'innerContent' => array(),
+			),
+		),
+		'innerContent' => array(
+			'<div class="wp-block-group">',
+			null,
+			'</div>'
+		),
+	);
+}
+add_filter( 'hooked_block_ockham/like-button', 'insert_like_button_after_post_content', 10, 3 );
