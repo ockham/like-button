5ce60403a330c3770e8ae5ae15f50497a39f63bb
Bernie Reiter <ockham@raz.or.at>
2024-01-10 20:18:37 +0100

    Try hooked_block filter with a Pattern block

--- like-button.php
+++ like-button.php
@@ -22,5 +22,34 @@
  */
 function create_block_like_button_block_init() {
 	register_block_type( __DIR__ . '/build' );
+
+	register_block_pattern(
+		'ockham/like-button-wrapper',
+		array(
+			'title'       => __( 'Like Button', 'like-button' ),
+			'description' => _x( 'A button to like content.', 'Block pattern description', 'like-button' ),
+			'content'     => '<!-- wp:group {"layout":{"type":"constrained"}} --><div class="wp-block-group"><!-- wp:ockham/like-button /--></div><!-- /wp:group -->',
+			'inserter'    => false
+		),
+	);
 }
 add_action( 'init', 'create_block_like_button_block_init' );
+
+function insert_like_button_pattern_after_post_content( $hooked_block, $position, $anchor_block ) {
+	if ( 'before' !== $position && 'after' !== $position ) {
+		return $hooked_block;
+	}
+
+	if ( 'core/post-content' !== $anchor_block['blockName'] ) {
+		return $hooked_block;
+	}
+
+	// We replace the "simple" block with a pattern that contains a Group block wrapper.
+	return array(
+		'blockName' => 'core/pattern',
+		'attrs'     => array(
+			'slug' => 'ockham/like-button-wrapper',
+		),
+	);
+}
+add_filter( 'hooked_block_ockham/like-button', 'insert_like_button_pattern_after_post_content', 10, 3 );
