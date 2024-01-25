8395d1a3d0a4bf6e3ba58076d564fa3414bf7e54
Bernie Reiter <ockham@raz.or.at>
2024-01-25 14:55:47 +0100

    Copy layout attribute from adjacent anchor block

--- like-button.php
+++ like-button.php
@@ -24,3 +24,19 @@ function create_block_like_button_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
 add_action( 'init', 'create_block_like_button_block_init' );
+
+function set_like_button_block_layout_attribute_based_on_adjacent_block( $hooked_block, $relative_position, $anchor_block ) {
+	// Is the hooked block adjacent to the anchor block?
+	if ( 'before' !== $relative_position && 'after' !== $relative_position ) {
+		return $hooked_block;
+	}
+
+	// Does the anchor block have a layout attribute?
+	if ( isset( $anchor_block['attrs']['layout'] ) ) {
+		// Copy the anchor block's layout attribute to the hooked block.
+		$hooked_block['attrs']['layout'] = $anchor_block['attrs']['layout'];
+	}
+
+	return $hooked_block;
+}
+add_filter( 'hooked_block_ockham/like-button', 'set_like_button_block_layout_attribute_based_on_adjacent_block', 10, 3 );
