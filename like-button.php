3cf74ec85c43849784855128514517df6940b07e
Bernie Reiter <ockham@raz.or.at>
2024-01-25 15:00:30 +0100

    Update required WP versions

--- like-button.php
+++ like-button.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Like Button
  * Description:       Example block scaffolded with Create Block tool.
- * Requires at least: 6.1
+ * Requires at least: 6.4
  * Requires PHP:      7.0
  * Version:           0.6.0
  * Author:            The WordPress Contributors
