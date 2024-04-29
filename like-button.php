62ba03730efcd7020e5f2c3412e84cd2866defd5
Bernie Reiter <ockham@raz.or.at>
2024-04-29 15:19:22 +0200

    Remove an unnecessary comment

--- like-button.php
+++ like-button.php
@@ -11,13 +11,6 @@
  * Text Domain:       like-button
  */
 
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/reference/functions/register_block_type/
- */
 function create_block_like_button_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
