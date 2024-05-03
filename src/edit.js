d0f15cffdf30e7f32135eeadd0e49b39aa3c21f6
Bernie Reiter <ockham@raz.or.at>
2024-05-03 11:40:13 +0200

    Remove now-obsolete editor styling

--- src/edit.js
+++ src/edit.js
@@ -13,14 +13,6 @@ import { __ } from '@wordpress/i18n';
  */
 import { useBlockProps } from '@wordpress/block-editor';
 
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
-import './editor.scss';
-
 import { ReactComponent as EmptyHeart } from '../public/images/empty-heart.svg';
 
 /**
